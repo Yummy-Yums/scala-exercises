e5fab300043960b06682d2383a889ee153528f6a
Saile Walderbs <sailewalderbs@gmail.com>
2024-10-22 16:48:18 +0000

    s-99 initial commit

---
+++ s-99/list-exercise.scala
@@ -0,0 +1,95 @@
+
+
+object Main extends App {
+
+
+  val e = List(1,2,3,4,5)
+
+  // find the last element of a list
+  val lastElement = e match {
+    case last :: Nil => last
+    case _ :: y => y.last
+    case Nil => throw new NoSuchElementException("List is empty")
+  }
+
+  // find the last but one element
+
+  val lastButOneElem = e match {
+    case penultimate :: _ :: Nil => penultimate
+    case _ :: tail => tail.init.last 
+    case _ => throw new NoSuchElementException("List is empty")
+  }
+
+  // find the Kth element of a list
+
+  def findKthElement[A](kth: Int, list: List[A]): A = {
+    var num = 0
+    var lis = list
+    
+    while(lis.nonEmpty){
+      if (num == kth) return lis.head
+      lis = lis.tail
+      num += 1
+    }
+
+    throw new NoSuchElementException("Index out of bounds")
+    
+  }
+
+  def isPalindrome[A](list: List[A]): Boolean = {
+    var second = List.empty[A]
+
+    var temp = list
+
+    while(temp.nonEmpty){
+      second = temp.head :: second
+      temp = temp.tail
+
+    }
+
+    return list == second
+  }
+
+  def isPalindrome2[A](list: List[A]): Boolean = {
+  val len = list.length
+  var firstHalf = list.take(len / 2)
+  var secondHalf = list.takeRight(len / 2).reverse
+
+  while (firstHalf.nonEmpty && secondHalf.nonEmpty) {
+    if (firstHalf.head != secondHalf.head) {
+      return false
+    }
+    firstHalf = firstHalf.tail
+    secondHalf = secondHalf.tail
+  }
+
+  true
+}
+
+  def flatten(list: List[Any]): List[Any] = {
+    
+    list match {
+      case Nil => Nil
+      case (x: List[Any]) :: xs => flatten(x) ::: flatten(xs)
+      case x :: xs =>  x :: flatten(xs)
+    }
+
+  }
+
+  def compressRecursive[A](ls: List[A]): List[A] = ls match {
+    case Nil       => Nil
+    case h :: tail => h :: compressRecursive(tail.dropWhile(_ == h))
+  }
+  println(compressRecursive(List(Symbol("a"), Symbol("a"), Symbol("a"), Symbol("a"), Symbol("b"), Symbol("c"), Symbol("c"), Symbol("a"), Symbol("a"), Symbol("d"), Symbol("e"), Symbol("e"), Symbol("e"), Symbol("e"))
+))
+
+  println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+  
+  println(findKthElement(2, List(1, 1, 2, 3, 5, 8)))
+  println(findKthElement(3, List("o", "p", "l", "a")))
+  println(isPalindrome(List(1, 2, 3, 2, 1)))
+  println(isPalindrome(List(1, 2, 3)))
+    
+  println("Hello world!")
+  
+}
